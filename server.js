56b9334228fa159654a2dc1b979302703d7cd358
kimberlyohq <e0424807@u.nus.edu>
2020-05-18 22:54:41 +0800

    rename db

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const db = process.env.DB;
+mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
