3a22065b52d6e6a1fbd910e68a313e49fe23ec2c
kimberlyohq <e0424807@u.nus.edu>
2020-05-20 12:04:13 +0800

    updated registerUser

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.route('/login').post((req, res) => {
             } else {
                 Bcrypt.compare(password, user.password, (err, result) => {
                     if (result) {
-                        return res.json('User found!');
+                        return res.send('User found!');
                     } else {
                         return res.send('Incorrect Password');
                     }
@@ -31,15 +31,21 @@ router.route('/login').post((req, res) => {
 router.route('/registerUser').post((req, res) => {
     const username = req.body.username;
     const email = req.body.email;
-    const password = Bcrypt.hashSync(req.body.password, 10); //hash the password
 
-    const newUser = new User({username, email, password});
+    User.findOne({username: username})
+        .then((user) => {
+            if (user) {
+                return res.send("User exists");
+            } else {
 
-    newUser.save()
-    .then(() => 
-        res.json('User added!')
-    )
-    .catch(err => res.status(400).json('Error: ' + err));
+                const password = Bcrypt.hashSync(req.body.password, 10); //hash the password
+                const newUser = new User({username, email, password});
+                
+                newUser.save()
+                        .then(() => res.send('User added!'))
+
+            }   
+        })
 });
 
 module.exports = router;
