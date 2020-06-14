c6f08ef6cfd2eb0d14122a6a95cefddc83e24a39
kimberlyohq <e0424807@u.nus.edu>
2020-06-14 21:39:16 +0800

    error handling

--- routes/search.js
+++ routes/search.js
@@ -4,47 +4,51 @@ const puppeteer = require('puppeteer');
 
 
 router.post('/', async (req, res) => {
-    console.log(req.body.productName);
-    let productName = req.body.productName;
+    console.log(req.body.name);
+    let productName = req.body.name;
 
     //replace space with -
     productName = productName.replace(/\s+/g, '-');
 
     let url = "https://incidecoder.com/products/" + productName;
 
-    const browser = await puppeteer.launch({headless : false});
+    const browser = await puppeteer.launch({headless : true});
     const page = await browser.newPage();
     await page.goto(url);
 
-    //extracting data
-    let data = await page.evaluate(() => {
-        //div with links 
-        //let ingredList = document.querySelector("#ingredlist-short");
-        let ingredList = document.querySelector("#showmore-section-ingredlist-short").innerText;
-        let productDetails = document.querySelector("#product-details").innerText;
-        return {ingredList, productDetails};
+    try {
+        //extracting data
+        let data = await page.evaluate(() => {
+            //div with links 
+            //let ingredList = document.querySelector("#ingredlist-short");
+            let ingredList = document.querySelector("#showmore-section-ingredlist-short").innerText;
+            let productDetails = document.querySelector("#product-details").innerText;
+            return {ingredList, productDetails};
 
-    })
+        })
 
-    let table = await page.evaluate(() => {
-        //retrieve table body
-        let table = document.querySelector(".product-skim tbody");
-        let rows = Array.from(table.children).slice(0, 10);
+        let table = await page.evaluate(() => {
+            //retrieve table body
+            let table = document.querySelector(".product-skim tbody");
+            let rows = Array.from(table.children).slice(0, 10);
+            
+            let data = rows.map(row => {
+                const columns = row.querySelectorAll('td');
+                return Array.from(columns, column => column.innerText);
+            });
+
+            return data;
         
-        let data = rows.map(row => {
-            const columns = row.querySelectorAll('td');
-            return Array.from(columns, column => column.innerText);
-          });
+        });
 
-        return data;
-    
-    });
+        browser.close();
 
-    console.log(table[0][0]);
+        return res.status(200).send({ingredients: data.ingredList, tableData: table});
 
-    browser.close();
+    } catch (err) {
+        return res.status(400).send("No Such Product Found");
+    }
 
-    return res.status(200).send({ingredients: data.ingredList, tableData: table});
 })
 
 module.exports = router;
